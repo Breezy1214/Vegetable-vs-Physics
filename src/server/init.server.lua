28fbdb181ce3cd02b627d0c64f75915814849dc1
devbreezy <nwabuikecharles@gmail.com>
2023-07-09 03:00:00 -0400

    set player collision. change later

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,5 +1,17 @@
 local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
 local ServerScriptService = game:GetService("ServerScriptService")
 
+game:GetService("Players").PlayerAdded:Connect(function(player)
+	player.CharacterAdded:Connect(function(char)
+        task.wait(1)
+		for _, part in char:GetDescendants() do
+			if part:IsA("BasePart") then
+				print("passed")
+				part.CollisionGroup = "Player"
+			end
+		end
+	end)
+end)
+
 Knit.AddServicesDeep(ServerScriptService:WaitForChild("Services"))
 Knit.Start():catch(warn)
